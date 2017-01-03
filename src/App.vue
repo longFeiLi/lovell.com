83181583ba85f02b9850b46883c999f8157a985d
longfei <longfei.li@seeed.cc>
2017-01-03 17:34:53 +0800

    增加了less修改了配置文件

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
   <div id="app">
-     <img src="./assets/logo.png">
-     <ul>
-       <li><router-link to="/" exact>Home</router-link></li>
-       <li><router-link to="/topics">Topics</router-link></li>
-       <li><router-link to="/counter">Counter</router-link></li>
-       <li><router-link to="/about">About</router-link></li>
-     </ul>
+      <div class="header-top">
+          <div class="top-menu"> <span class="menu">
+              <ul>
+                <li><router-link to="/" exact>Home</router-link></li>
+                <li><router-link to="/topics">Topics</router-link></li>
+                <li><router-link to="/counter">Counter</router-link></li>
+                <li><router-link to="/about">About</router-link></li>
+              </ul>
+          </div>
+            <div class="search">
+               <input type="text" value="Search..">
+                <input type="submit" value="">
+            </div>
+            <div class="clearfix"></div>
+      </div>
      <router-view class="view"></router-view>
   </div>
 </template>
-
-<style>
+<style lang="less">
   body {
     font-family: Helvetica, sans-serif;
+    .router-link-active{
+      color: red;
+    }
   }
-  .router-link-active{
-    color: red ;
-  }
+  
+
+  
 </style>
